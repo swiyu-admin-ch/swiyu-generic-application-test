deadf824856a9090847df59e52cca19d33889137
Karim Kummer <karim.kummer@bit.admin.ch>
2025-10-24 11:24:12 +0200

    Revert mock server config to avoid token refresh fail

--- test-wallet-library/src/main/java/ch/admin/bj/swiyu/swiyu_test_wallet/config/MockServerContainerConfig.java
+++ test-wallet-library/src/main/java/ch/admin/bj/swiyu/swiyu_test_wallet/config/MockServerContainerConfig.java
@@ -1,33 +1,33 @@
 package ch.admin.bj.swiyu.swiyu_test_wallet.config;
 
+import ch.admin.bj.swiyu.swiyu_test_wallet.issuer.IssuerConfig;
 import lombok.experimental.UtilityClass;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.MockServerContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
-import java.time.Duration;
+import static ch.admin.bj.swiyu.swiyu_test_wallet.config.MockServerClientConfig.createMockServerClient;
 
 @UtilityClass
 public class MockServerContainerConfig {
 
-    public static MockServerContainer createAndStartMockServerContainer(Network network, int port) {
+    public static MockServerContainer createAndStartMockServerContainer(Network network, IssuerConfig config) {
         DockerImageName imageName = DockerImageName
                 .parse("mockserver/mockserver")
                 .withTag("5.15.0");
 
-        try (MockServerContainer container = new MockServerContainer(imageName)) {
-            container.withExposedPorts(port)
-                    .withNetwork(network)
-                    .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("mockserver")))
-                    .withNetworkAliases("mockserver")
-                    .waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(60)));
+        var container = new MockServerContainer(imageName)
+                .withExposedPorts(1080)
+                .withNetwork(network)
+                .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("mockserver")))
+                .withNetworkAliases("mockserver");
 
-            container.start();
+        container.start();
 
-            return container;
-        }
+        createMockServerClient(container, config);
+
+        return container;
     }
 }
