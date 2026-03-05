824f42aa58ad45a5eb5fb210fb789d0a1547f204
Karim Kummer <karim.kummer@bit.admin.ch>
2026-03-05 15:24:09 +0100

    Remove signed metadata test fix will be available later

--- test-wallet-library/src/main/java/ch/admin/bj/swiyu/swiyu_test_wallet/test_support/issuer_metadata/IssuerMetadataAssert.java
+++ test-wallet-library/src/main/java/ch/admin/bj/swiyu/swiyu_test_wallet/test_support/issuer_metadata/IssuerMetadataAssert.java
@@ -170,14 +170,4 @@ public final class IssuerMetadataAssert {
 
         return this;
     }
-
-    public IssuerMetadataAssert hasTenantId() {
-        final String credentialIssuer = data.getCredentialIssuer();
-
-        Assertions.assertThat(credentialIssuer)
-                .as("Credential issuer should contain a tenantId UUID at the end of the URL")
-                .matches(".*/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$");
-
-        return this;
-    }
 }
