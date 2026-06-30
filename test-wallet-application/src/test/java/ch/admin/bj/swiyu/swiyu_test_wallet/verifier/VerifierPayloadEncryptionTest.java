c3ea9ae7b8458594616f6252cbd76db0dcb42c76
KarimKummerBIT <karim.kummer@bit.admin.ch>
2026-06-30 08:38:27 +0200

    chore(EIDOMNI-654): Disable test as feature not available yet (#252)

--- test-wallet-application/src/test/java/ch/admin/bj/swiyu/swiyu_test_wallet/verifier/VerifierPayloadEncryptionTest.java
+++ test-wallet-application/src/test/java/ch/admin/bj/swiyu/swiyu_test_wallet/verifier/VerifierPayloadEncryptionTest.java
@@ -64,7 +64,7 @@ class VerifierPayloadEncryptionTest extends BaseTest {
     @Tag(ReportingTags.UCV_O2A)
     @Tag(ReportingTags.HAPPY_PATH)
     @DisableIfImageTag(
-            issuer = {ImageTags.STABLE, ImageTags.RC},
+            issuer = {ImageTags.STABLE, ImageTags.RC, ImageTags.STAGING},
             reason = "The fix about alg in jwk keys is not yet available on disabled tags"
     )
     void verifyDCQL_whenEncryptionRequired_thenSuccess(final String supportedMetadataId) {
